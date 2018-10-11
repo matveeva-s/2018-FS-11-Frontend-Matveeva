cd8a52a21393d6d555256e0aac83849b0b507588
Svetlana Matveeva <matveeva.svetlana@gmail.com>
2018-10-11 21:43:26 +0300

    hw1

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,6 +1,6 @@
 const say = function() {
 	var div = document.createElement('div');
-	div.innerHTML = "<strong>Hello</strong>, have a nice day :)";
+	div.innerHTML = "<strong>Hello teacher</strong>, have a nice day :)";
 	div.style.width = 750;
 	div.style.fontSize = 45;
 	div.style.padding =150;
