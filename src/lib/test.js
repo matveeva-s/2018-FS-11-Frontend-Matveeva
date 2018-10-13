bbb77197b15bc912d3688a081666eb5f3da62983
Svetlana Matveeva <matveeva.svetlana@gmail.com>
2018-10-13 19:36:43 +0300

    fixed homework

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,10 +1,7 @@
 const say = function() {
 	var div = document.createElement('div');
 	div.innerHTML = "<strong>Hello teacher</strong>, have a nice day :)";
-	div.style.width = 750;
-	div.style.fontSize = 45;
-	div.style.padding =150;
-	div.style.backgroundColor = "#7FFF00";
+	div.classList.add("mydiv");
 	document.body.appendChild(div);
 };
 
