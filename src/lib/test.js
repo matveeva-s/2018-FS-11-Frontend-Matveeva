37f1307169602f4c21a59188dbbf558885eeaab4
Svetlana Matveeva <matveeva.svetlana@gmail.com>
2018-10-15 17:54:20 +0300

    update

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,4 +1,4 @@
-const say = function() {
+const say = function() { 
 	var div = document.createElement('div');
 	div.innerHTML = "<strong>Hello teacher</strong>, have a nice day :)";
 	div.classList.add("mydiv");
