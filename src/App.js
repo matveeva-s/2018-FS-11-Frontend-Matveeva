b38fccd40d06009d95a756b495348eaafa6200fa
matveeva-s <matveeva.svetlana@gmail.com>
2018-11-27 00:27:50 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import './App.css';
-import avatar from './images/avatar.png'
+import avatar1 from './images/morty.svg'
+import avatar2 from './images/rick.svg'
+import { Link } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
-class Messenger extends React.Component {
+
+const App = () => (
+    <main>
+        <Switch>
+            <Route exact path='/' component={Chats}/>
+            <Route path='/chat1' component={Chat1}/>
+            <Route path='/chat2' component={Chat2}/>
+        </Switch>
+    </main>
+);
+
+class Chats extends React.Component {
+    render() {
+        return (
+            <div className="Messenger">
+                <div className="header"/>
+                <TitleChats />
+                <div className="result-scroll">
+                    <div id="users" className="users">
+                      <div id="chat1">
+                          <img className="avatar" src={avatar1} align="left"/>
+                          <Link to='/chat1'><h4>Morty Smith</h4></Link>
+                      </div>
+                      <hr width="330px" size="1px" color="#8e24aa" align="right"/>
+                      <div id="chat2">
+                          <img className="avatar" src={avatar2}/>
+                          <Link to='/chat2'><h4>Rick Sanchez</h4></Link>
+                      </div>
+                      <hr width="330px" size="1px" color="#8e24aa" align="right"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Chat1 extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
@@ -55,7 +95,7 @@ class Messenger extends React.Component {
     return (
       <div className="Messenger">
         <div className="header"/>
-        <Title />
+        <Title1/>
         <div className="result-scroll">
             <div id="result" className="result"/>
         </div>
@@ -68,17 +108,112 @@ class Messenger extends React.Component {
     );
   }
 }
-function Title() {
+
+class Chat2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            file: ''
+        };
+        this._addHandlers();
+
+    }
+    _addHandlers() {
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleAddFile = this.handleAddFile.bind(this);
+        this.handleAddImage = this.handleAddImage.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    handleAddFile(event) {
+        this.state.file = event.target.files[0];
+        const fileDiv = document.createElement('div');
+        fileDiv.className = 'cloud';
+        fileDiv.innerHTML = `<font color = 'black'>Added file:</font>` + this.state.file.name;
+        document.getElementById('result').appendChild(fileDiv);
+    }
+    handleAddImage(event) {
+        this.state.file = event.target.files[0];
+        const url = URL.createObjectURL(this.state.file);
+        const imageDiv = document.createElement('img');
+        imageDiv.className = 'preview';
+        imageDiv.src = url;
+        document.getElementById('result').appendChild(imageDiv);
+    }
+    handleSubmit(event) {
+        if (this.state.value === '') {
+            event.preventDefault();
+            return false;
+        }
+        const message = document.createElement('div');
+        message.className = 'cloud';
+        message.innerText = this.state.value;
+        document.getElementById('result').appendChild(message);
+        document.getElementById('form-input').value = '';
+        this.state.value = '';
+        event.preventDefault();
+        return false;
+    }
+
+    render() {
+        return (
+            <div className="Messenger">
+                <div className="header"/>
+                <Title2 />
+                <div className="result-scroll">
+                    <div id="result" className="result"/>
+                </div>
+                <input id="addFile" className="addFileButton" type="file" onChange={this.handleAddFile}/>
+                <input id="addImage" className="addImageButton" type="file" multiple accept="image/*" onChange={this.handleAddImage}/>
+                <form onSubmit={this.handleSubmit}>
+                    <input id="form-input" placeholder="Write a message here..." value = {this.state.value} onChange={this.handleChange}/>
+                </form>
+            </div>
+        );
+    }
+}
+
+function TitleChats() {
     return(
         <div className="title">
-            <img className="avatar" src={avatar} alt="avatar"/>
+            <input id="allMenuButton" type="button"/>
+            <h2>Please select chat:</h2>
+            <input id="searchButton" type="button"/>
+        </div>
+
+    )
+}
+function Title1() {
+    return(
+        <div className="title">
+            <input id="backButton" type="button" />
+            <img className="avatar" src={avatar1} alt="avatar"/>
             <p>
-                <span className="name">Jennifer</span>
+                <span className="name">Morty Smith</span>
+                <br/>online
+            </p>
+            <input id="searchButton" type="button"/>
+            <input id="menuButton" type="button"/>
+        </div>
+    )
+}
+function Title2() {
+    return(
+        <div className="title">
+            <input id="backButton" type="button" onClick='location.href="http://codehelper.ru/"'/>
+            <img className="avatar" src={avatar2} alt="avatar"/>
+            <p>
+                <span className="name">Rick Sanchez</span>
                 <br/>was online 2 hours ago
             </p>
+            <input id="searchButton" type="button"/>
+            <input id="menuButton" type="button"/>
         </div>
     )
 }
 
 
-export default Messenger;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Messenger from './App';
+import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root'))
+
+/*ReactDOM.render(
     <Messenger />,
     document.getElementById('root')
-);
-/* HELLO WORLS TEST CODE
-ReactDOM.render(
-<h1>Hello, world!</h1>,
-document.getElementById('root')
-);
-*/
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+);*/
+
 serviceWorker.unregister();
